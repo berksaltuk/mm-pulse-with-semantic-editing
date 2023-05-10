ee70cda21980ddb154cde96099c608d8a8759d18
berksaltuk <saltuk.yilmaz@ug.bilkent.edu.tr>
2023-05-10 16:38:38 +0300

    Multi-Modal PULSE initial

--- PULSE.py
+++ PULSE.py
@@ -1,4 +1,4 @@
-from stylegan import G_synthesis,G_mapping
+from stylegan import G_synthesis, G_mapping
 from dataclasses import dataclass
 from SphericalOptimizer import SphericalOptimizer
 from pathlib import Path
@@ -18,10 +18,9 @@ class PULSE(torch.nn.Module):
         self.verbose = verbose
 
         cache_dir = Path(cache_dir)
-        cache_dir.mkdir(parents=True, exist_ok = True)
-        if self.verbose: print("Loading Synthesis Network")
-        with open_url("https://drive.google.com/uc?id=1TCViX1YpQyRsklTVYEJwdbmK91vklCo8", cache_dir=cache_dir, verbose=verbose) as f:
-            self.synthesis.load_state_dict(torch.load(f))
+        cache_dir.mkdir(parents=True, exist_ok=True)
+        if self.verbose:
+            print("Loading Synthesis Network")
 
         for param in self.synthesis.parameters():
             param.requires_grad = False
@@ -31,20 +30,21 @@ class PULSE(torch.nn.Module):
         if Path("gaussian_fit.pt").exists():
             self.gaussian_fit = torch.load("gaussian_fit.pt")
         else:
-            if self.verbose: print("\tLoading Mapping Network")
+            if self.verbose:
+                print("\tLoading Mapping Network")
             mapping = G_mapping().cuda()
-
-            with open_url("https://drive.google.com/uc?id=14R6iHGf5iuVx3DMNsACAl7eBr7Vdpd0k", cache_dir=cache_dir, verbose=verbose) as f:
-                    mapping.load_state_dict(torch.load(f))
-
-            if self.verbose: print("\tRunning Mapping Network")
+            if self.verbose:
+                print("\tRunning Mapping Network")
             with torch.no_grad():
                 torch.manual_seed(0)
-                latent = torch.randn((1000000,512),dtype=torch.float32, device="cuda")
+                latent = torch.randn(
+                    (1000000, 512), dtype=torch.float32, device="cuda")
                 latent_out = torch.nn.LeakyReLU(5)(mapping(latent))
-                self.gaussian_fit = {"mean": latent_out.mean(0), "std": latent_out.std(0)}
-                torch.save(self.gaussian_fit,"gaussian_fit.pt")
-                if self.verbose: print("\tSaved \"gaussian_fit.pt\"")
+                self.gaussian_fit = {"mean": latent_out.mean(
+                    0), "std": latent_out.std(0)}
+                torch.save(self.gaussian_fit, "gaussian_fit.pt")
+                if self.verbose:
+                    print("\tSaved \"gaussian_fit.pt\"")
 
     def forward(self, ref_im,
                 seed,
@@ -69,7 +69,7 @@ class PULSE(torch.nn.Module):
         batch_size = ref_im.shape[0]
 
         # Generate latent tensor
-        if(tile_latent):
+        if (tile_latent):
             latent = torch.randn(
                 (batch_size, 1, 512), dtype=torch.float, requires_grad=True, device='cuda')
         else:
@@ -77,17 +77,17 @@ class PULSE(torch.nn.Module):
                 (batch_size, 18, 512), dtype=torch.float, requires_grad=True, device='cuda')
 
         # Generate list of noise tensors
-        noise = [] # stores all of the noise tensors
+        noise = []  # stores all of the noise tensors
         noise_vars = []  # stores the noise tensors that we want to optimize on
 
         for i in range(18):
             # dimension of the ith noise tensor
             res = (batch_size, 1, 2**(i//2+2), 2**(i//2+2))
 
-            if(noise_type == 'zero' or i in [int(layer) for layer in bad_noise_layers.split('.')]):
+            if (noise_type == 'zero' or i in [int(layer) for layer in bad_noise_layers.split('.')]):
                 new_noise = torch.zeros(res, dtype=torch.float, device='cuda')
                 new_noise.requires_grad = False
-            elif(noise_type == 'fixed'):
+            elif (noise_type == 'fixed'):
                 new_noise = torch.randn(res, dtype=torch.float, device='cuda')
                 new_noise.requires_grad = False
             elif (noise_type == 'trainable'):
@@ -120,7 +120,7 @@ class PULSE(torch.nn.Module):
         }
         schedule_func = schedule_dict[lr_schedule]
         scheduler = torch.optim.lr_scheduler.LambdaLR(opt.opt, schedule_func)
-        
+
         loss_builder = LossBuilder(ref_im, loss_str, eps).cuda()
 
         min_loss = np.inf
@@ -129,8 +129,8 @@ class PULSE(torch.nn.Module):
         start_t = time.time()
         gen_im = None
 
-
-        if self.verbose: print("Optimizing")
+        if self.verbose:
+            print("Optimizing")
         for j in range(steps):
             opt.opt.zero_grad()
 
@@ -141,7 +141,8 @@ class PULSE(torch.nn.Module):
                 latent_in = latent
 
             # Apply learned linear mapping to match latent distribution to that of the mapping network
-            latent_in = self.lrelu(latent_in*self.gaussian_fit["std"] + self.gaussian_fit["mean"])
+            latent_in = self.lrelu(
+                latent_in*self.gaussian_fit["std"] + self.gaussian_fit["mean"])
 
             # Normalize image to [0,1] instead of [-1,1]
             gen_im = (self.synthesis(latent_in, noise)+1)/2
@@ -151,20 +152,20 @@ class PULSE(torch.nn.Module):
             loss_dict['TOTAL'] = loss
 
             # Save best summary for log
-            if(loss < min_loss):
+            if (loss < min_loss):
                 min_loss = loss
                 best_summary = f'BEST ({j+1}) | '+' | '.join(
-                [f'{x}: {y:.4f}' for x, y in loss_dict.items()])
+                    [f'{x}: {y:.4f}' for x, y in loss_dict.items()])
                 best_im = gen_im.clone()
 
             loss_l2 = loss_dict['L2']
 
-            if(loss_l2 < min_l2):
+            if (loss_l2 < min_l2):
                 min_l2 = loss_l2
 
             # Save intermediate HR and LR images
-            if(save_intermediate):
-                yield (best_im.cpu().detach().clamp(0, 1),loss_builder.D(best_im).cpu().detach().clamp(0, 1))
+            if (save_intermediate):
+                yield (best_im.cpu().detach().clamp(0, 1), loss_builder.D(best_im).cpu().detach().clamp(0, 1))
 
             loss.backward()
             opt.step()
@@ -172,8 +173,9 @@ class PULSE(torch.nn.Module):
 
         total_t = time.time()-start_t
         current_info = f' | time: {total_t:.1f} | it/s: {(j+1)/total_t:.2f} | batchsize: {batch_size}'
-        if self.verbose: print(best_summary+current_info)
-        if(min_l2 <= eps):
-            yield (gen_im.clone().cpu().detach().clamp(0, 1),loss_builder.D(best_im).cpu().detach().clamp(0, 1))
+        if self.verbose:
+            print(best_summary+current_info)
+        if (min_l2 <= eps):
+            yield (gen_im.clone().cpu().detach().clamp(0, 1), loss_builder.D(best_im).cpu().detach().clamp(0, 1))
         else:
             print("Could not find a face that downscales correctly within epsilon")
